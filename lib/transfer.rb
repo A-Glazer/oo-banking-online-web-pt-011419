2a46ce17bf965bd07b20608e41a924dda7ecf8b8
A Glazer <avigayilglazer@gmail.com>
2019-03-07 21:14:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,14 +9,20 @@ class Transfer
   end
 
   def valid?
+<<<<<<< HEAD
     if sender.valid? == true && receiver.valid? == true &&    @sender.balance >= @amount
       true
     else
       false
+=======
+    if sender.valid? == true && receiver.valid? == true
+    true
+>>>>>>> c8110f60a5460aad5aef53e62616bac7aa6ea413
     end
   end
 
   def execute_transaction
+<<<<<<< HEAD
     if self.valid? == false || @status == "complete"
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
@@ -35,4 +41,16 @@ class Transfer
       @status = "reversed"
     end
   end
+=======
+    if valid? == true && @status == "pending"
+      @sender.balance -= amount
+      @receiver.balance += amount
+      @status = "complete"
+    else
+      "You already did a transaction"
+    end
+
+  end
+
+>>>>>>> c8110f60a5460aad5aef53e62616bac7aa6ea413
 end
